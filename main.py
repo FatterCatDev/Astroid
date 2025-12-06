3443deb3055a924a4ef77a157aafb80c8471ae8c
FatterCatDev <kevin@semutacasino.com>
2025-12-05 19:53:29 -0800

    added window exit

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # open window
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
