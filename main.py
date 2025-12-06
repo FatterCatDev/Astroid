d95033237f5e64276e76cca0a69578f3ad9abb98
FatterCatDev <kevin@semutacasino.com>
2025-12-05 20:10:42 -0800

    clock fix

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     # open window
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     # game loop
     while True:
@@ -19,6 +21,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(120)
+        dt = clock.tick(120)
+        print(dt)
 
 
 if __name__ == "__main__":
