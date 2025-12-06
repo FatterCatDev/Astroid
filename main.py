643aad39577292fd37afa35d4d7808a9d51dd8c1
FatterCatDev <kevin@semutacasino.com>
2025-12-05 20:57:31 -0800

    Added player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from logger import log_state
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -12,6 +13,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # game loop
     while True:
@@ -19,10 +21,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
-        clock.tick(120)
-        dt = clock.tick(120)
+        clock.tick(60)
+        dt = clock.tick(60)
         print(dt)
 
 
